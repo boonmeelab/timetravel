c59a99d518e0193e9a72bea06dadbf8d3e1d661f
Rapee Suveeranont <iiieyes@gmail.com>
2014-07-13 19:10:14 +0700

    Updated for Gettyimages Japan

--- public/js/render.js
+++ public/js/render.js
@@ -1,4 +1,4 @@
-
+var stageTop = 30;
 var camera, scene, renderer;
 var cube;
 var controls;
@@ -7,7 +7,7 @@ var offset = 0;
 var speed = 0;
 var acc = 0.97;
 
-var keyboard = new KeyboardState();
+var keyboard = new THREEx.KeyboardState();
 var projector, mouse = { x: 0, y: 0 }, INTERSECTED;
 
 var objectList;
@@ -18,9 +18,9 @@ var current_object_index;
 init();
 animate();
 
-const FADEOUT_DISTANCE = 30;
-const INFOBOX_RANGE_CENTER_POSITION = 10;
-const INFOBOX_RANGE_DISTANCE = 76;
+var FADEOUT_DISTANCE = 30;
+var INFOBOX_RANGE_CENTER_POSITION = 10;
+var INFOBOX_RANGE_DISTANCE = 76;
 
 function addOnTimeline(obj, d1, d2) {
   objectList.add(obj);
@@ -61,9 +61,15 @@ function init() {
   window.addEventListener( 'resize', onWindowResize, false );
 
   renderer = new THREE.WebGLRenderer( { alpha: true } );
+  if (window.WebGLRenderingContext)
+    renderer = new THREE.WebGLRenderer( { alpha: true } );
+  else
+    renderer = new THREE.CanvasRenderer( { alpha: true } );
+
+
   // renderer.setClearColor( 0xeeeeee, 1);
   renderer.setClearColor( 0x000000, 0);
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setSize( window.innerWidth, window.innerHeight - stageTop );
   document.getElementById( 'stage' ).appendChild( renderer.domElement );
 
   camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 3000 );
@@ -97,7 +103,7 @@ function onDocumentMouseMove( event )
   // event.preventDefault();
 
   // update sprite position
-  updateInfoBox({ x: event.clientX, y: event.clientY });
+  // updateInfoBox({ x: event.clientX, y: event.clientY });
   // sprite1.position.set( event.clientX, event.clientY - 20, 0 );
 
   // update the mouse variable
@@ -172,14 +178,15 @@ function createSprite(imgObj, params) {
 function createTextMarker(str) {
   var group = new THREE.Object3D();
   var text = createText( str, {
-      fontsize: 32,
+      fontsize: 64,
       fontface: 'Abel',
       backgroundColor: {r:237, g:28, b:36, a:1.0},//0xed1c24
       borderColor: {r:237, g:28, b:36, a:1.0},//0xed1c24
     } );
-  text.position.x = 37;
-  text.position.y = -10;
+  text.position.x = 37 - 24;
+  text.position.y = -10 + 15;
   text.position.z = 0;
+
   var line = createPlane({ width: 100, height: 3, position: { x: 0, y: .1, z: 0 }, rotation: { x: Math.PI/2 }, color: 0x000000});
 
   group.add(text);
@@ -239,7 +246,9 @@ function createText( message, parameters )
   var spriteMaterial = new THREE.SpriteMaterial(
     { map: texture, useScreenCoordinates: false, alignment: spriteAlignment } );
   var sprite = new THREE.Sprite( spriteMaterial );
-  sprite.scale.set(100,50,1.0);
+  // sprite.scale.set(100,50,1.0);
+  sprite.scale.set(50,25,1.0);
+  sprite.rotation.set(Math.PI/2,0,0);
   return sprite;
 }
 
@@ -248,7 +257,7 @@ function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setSize( window.innerWidth, window.innerHeight - stageTop);
 
 }
 
@@ -264,7 +273,7 @@ function animate() {
 }
 
 function keyUpdate() {
-  keyboard.update();
+  // keyboard.update();
   if ( keyboard.pressed("up") ) {
     pressNextItem();
   }
@@ -329,7 +338,7 @@ function startTween(from, to) {
 }
 function stopTween() {
   if (tween && tween.isPlaying) {
-    console.log('stop tweening')
+    // console.log('stop tweening')
     tween.stop();
     tween.isPlaying = false;
   }
@@ -428,10 +437,242 @@ function worldUpdate() {
     if(boxShowObj){
       updateInfoBox({
         show: true,
+        id: boxShowObj.imgObj.ImageId,
         title: boxShowObj.imgObj.Title,
         artist: boxShowObj.imgObj.Artist,
         caption: boxShowObj.imgObj.Caption
       });
+    } else {
+      updateInfoBox({
+        show: false
+      });
     }
   }
 }
+
+
+
+
+
+
+/*
+ * Search
+ */
+
+$(function() {
+  var requestId = null;
+  var searchResults;
+  var spin = new Spinner({color:'#666', left: '0', top: '10px', lines: 11, radius: 4, length: 2, width: 1.5});
+
+  // controller buttons next/prev
+  $('#controller')
+  .on('click', '.next-btn', function(e) {
+    e.preventDefault();
+    pressNextItem();
+  })
+  .on('click', '.prev-btn', function(e) {
+    e.preventDefault();
+    pressPrevItem();
+  });
+
+  // create slider with multihanders for year range search.
+  var current_year = new Date().getFullYear();
+  $('#year-range-slider').slider({
+    range: true,
+    // gettyimage has image created since 1753, but it makes our slider range too long, we change to later year.
+    min: 1900,
+    max: current_year,
+    values: [ 1972, 2014],
+    slide: function(event, ui){
+      // showing total year range
+      var text = ui.values[0] + ' - ' + ui.values[1];
+      $('#startdate').val(ui.values[0]);
+      $('#enddate').val(ui.values[1]);
+      $('#year-range-text').text(text);
+    }
+  });
+  $('#year-range-text').text( $('#year-range-slider').slider('values', 0 ) + ' - ' + $('#year-range-slider').slider('values', 1 ));
+
+  $('#form-search').on('submit', function(e) {
+    // hide old info. when search for new query
+    updateInfoBox({ show: false });
+    e.preventDefault();
+    $('.spinner').removeClass('hidden');
+    resetTimeline();
+    searchResults = [];
+
+    countSearchResultUp(0, false);
+    $('.loading-text').removeClass('hidden');
+    spin.spin($('.status')[0]);
+    $('.result-text').addClass('hidden');
+
+    var masterdata = $('#form-search').serializeObject();
+    masterdata.startdate = masterdata.startdate || $('#year-range-slider').slider('values',0)+'-01-01';
+    masterdata.enddate = masterdata.enddate || $('#year-range-slider').slider('values',1)+'-01-01';
+    var startyear = +masterdata.startdate.split('-')[0];
+    var endyear = +masterdata.enddate.split('-')[0];
+    var yearlist = [];
+    // generate each year
+    var range = endyear-startyear;
+    var step = parseInt(range/5);
+    for (var i=0; i<endyear-startyear; i+=step) {
+      yearlist.push(startyear+i);
+    }
+
+    // event hit: search submit
+    ga('send', 'event', 'search', 'submit', values(masterdata));
+
+    // itemCount can be only --> 1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 25, 30, 50, 60, 75
+    // limited by gettyimage api
+    var itemCount = step*5;
+    var limitedCount = [1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 25, 30, 50, 60, 75]
+    for( var i =0, limitedLength = limitedCount.length;i<limitedLength;i++){
+      if(limitedCount[i]>=itemCount){
+        itemCount=limitedCount[i];
+        break;
+      }
+    }
+    var reqId = requestId = Math.random();
+    var year_mark = null;
+    async.mapSeries(yearlist,
+      function(year, cb) {
+        if (reqId != requestId) {
+          // console.log('Cancel expired request')
+          return cb();
+        }
+
+        var data = clone(masterdata);
+        data.startdate = year+'-01-01';
+        var nextstep = year+step-1;
+        if(nextstep<endyear)
+          data.enddate = nextstep+'-12-31';
+        else data.enddate = endyear + '-12-31';
+
+        data.itemperpage = itemCount;
+
+        $.ajax({
+          url: '/search',
+          type: 'GET',
+          dataType: 'json',
+          data: data,
+          resetForm : true
+        })
+        .done(function(data){
+          if (reqId != requestId) return;
+
+            var itemList = [];
+            var imageArray = data.SearchForImagesResult&&data.SearchForImagesResult.Images||[];
+
+            $.each(imageArray,function(index, image){
+              var date = /\/Date\(([0-9]+).*\)\//g.exec(image.DateCreated);
+              if (date && date[1]) image.date = new Date(+date[1]);
+              itemList.push(image);
+            });
+
+            itemList.sort(function(a, b) {
+              return +a.date < +b.date ? -1 : 1;
+            });
+
+            // photos
+            itemList.forEach(function(image) {
+              var year = image.date&&image.date.getFullYear();
+              // year mark
+              if (typeof year === 'number' && year != year_mark) {
+                year_mark = year;
+                addOnTimeline(createTextMarker(year), 90, 30);
+              }
+
+              var w = +image.MaxImageResolutionWidth || 50;
+              var h = +image.MaxImageResolutionHeight || 50;
+              var ratio = Math.min(w, h)/Math.max(w, h);
+              var size = 60;
+              w2 = w > h ? size : size * ratio;
+              h2 = w > h ? size * ratio : size;
+              addOnTimeline(createSprite(image, {width: w2, height: h2, service: 'getty'}));
+            });
+
+            searchResults = searchResults.concat(itemList);
+
+            console.log('result', searchResults.length)
+            countSearchResultUp(searchResults.length, true);
+            // $('.result-text').spin({color:'#fff', lines: 12});
+            // spin.spin($('.result-text')[0])
+            // $('.loading-text').addClass('hidden');
+            // $('.result-text').removeClass('hidden').find('.count').text(searchResults.length);
+        })
+        .fail(function(xhr){
+          console.error(xhr);
+        })
+        .always(function() {
+          cb();
+        });
+      },
+
+      function(err, result) {
+        if (reqId === requestId) {
+          // $('.spinner').addClass('hidden');
+          spin.stop();
+          $('.loading-text').addClass('hidden');
+          $('.result-text').removeClass('hidden');
+          countSearchResultUp(searchResults.length, false);
+        }
+      }
+    )
+  });
+
+  // submit once on pageload
+  $('#form-search').trigger('submit');
+
+  // focus on search box
+  $('input[name="query"]').trigger('focus');
+
+});
+
+var countSearchResultUp_timer;
+function countSearchResultUp(count, animate) {
+  if (count > 0) {
+    $('#controller').removeClass('hidden');
+  }
+
+  if (!animate) {
+    $('.result-text').removeClass('hidden').find('.count').text(count);
+    return;
+  }
+
+  var now = +$('.result-text .count').text();
+  var dif = count - now;
+  if (countSearchResultUp_timer) clearTimeout(countSearchResultUp_timer);
+  countUp();
+  $('.loading-text').addClass('hidden');
+  $('.result-text').removeClass('hidden');
+
+  function countUp() {
+    if (now < count) $('.result-text .count').text(++now);
+    if (now < count) countSearchResultUp_timer = setTimeout(countUp, Math.random()*40+10);
+  }
+}
+
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
+function values(obj) {
+  return typeof obj !== 'object' ? '' : Object.keys(obj).map(function(key){ return obj[key]; }).join(',');
+}
+
+function clone(obj) {
+  return JSON.parse(JSON.stringify(obj));
+};
