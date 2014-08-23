83aaccae93aa1a06aa627a3e96c6e08bb60992d9
ParNurZeal <parnurzeal@gmail.com>
2014-08-23 21:25:27 +0900

    Added ie class in body instead of html tag

--- public/js/render.js
+++ public/js/render.js
@@ -1023,13 +1023,13 @@ NoWebGLTimeTravel.prototype.pressPrevItem = _.throttle(function() {
 
 // Start App
 var TT;
-if (!document.addEventListener || $('html').is('.ie8') || location.search.indexOf('mode=old') >= 0) {
-  console.log('ie8: ',$('html').is('.ie8'));
-  console.log('ie9: ',$('html').is('.ie9'));
+if (!document.addEventListener || $('body').is('.ie8') || location.search.indexOf('mode=old') >= 0) {
+  console.log('ie8: ',$('body').is('.ie8'));
+  console.log('ie9: ',$('body').is('.ie9'));
   TT = new NoWebGLTimeTravel();
 } else {
-  console.log('ie8: ',$('html').is('.ie8'));
-  console.log('ie9: ',$('html').is('.ie9'));
+  console.log('ie8: ',$('body').is('.ie8'));
+  console.log('ie9: ',$('body').is('.ie9'));
   TT = new WebGLTimeTravel();
 }
 
